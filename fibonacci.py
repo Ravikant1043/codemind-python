d16a9c91914270e32f8ab414bb53eb0228c45231
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-04-24 01:46:57 +0530

    def fib(n):     a=0     b=1     i=0     print(a,b,end=" ")     while n-2:         c=a+b         print(c,end=" ")         a=b         b=c         n-=1
    n=int(input())
    fib(n)

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fib(n):
+    a=0
+    b=1
+    i=0
+    print(a,b,end=" ")
+    while n-2:
+        c=a+b
+        print(c,end=" ")
+        a=b
+        b=c
+        n-=1
+    
+n=int(input())
+fib(n)
