89b972f8d8a3553fc6fee18f1f3cdae231687ad7
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-04-25 22:47:12 +0530

    n=int(input()) k=n+64 for i in range(1,n+1):     for j in range(i,n+1):         print(chr(k),end=" ")     k-=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,7 @@
 n=int(input())
+k=n+64
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        if (i==j or j==1 or j==n):
-            print("* ",end="")
-        else:
-            print("  ",end="")
+    for j in range(i,n+1):
+        print(chr(k),end=" ")
+    k-=1
     print()
