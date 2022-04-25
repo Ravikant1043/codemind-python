74b1d4d9c6198854e026398d2ecd81d668ec5ddc
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-04-25 22:14:11 +0530

    n=int(input()) t=n for i in range(1,n+1):     for j in range(t-1):         print(" ",end="")     t-=1     for j in range(2*i-1):         print(i,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,10 @@
 n=int(input())
-for i in range(65,65+n):
-    for j in range(n):
-        print(chr(i),end=" ")
+t=n
+for i in range(1,n+1):
+    for j in range(t-1):
+        print(" ",end="")
+    t-=1
+    for j in range(2*i-1):
+        print(i,end="")
     print()
+    
