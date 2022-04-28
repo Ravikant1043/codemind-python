d3b3b7da9d9980f6cea4356271ac791486c34aa7
Ravikant Sharma <97437176+Ravikant1043@users.noreply.github.com>
2022-04-28 19:12:23 +0530

    import math as m def root(n):     k=m.ceil(m.sqrt(n))     if(k**2==n):         return True     else:         return False n=int(input()) if(root(n)==1):     print("True") else:     print("False")

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,7 +1,12 @@
-import math
-a=int(input())
-m=math.ceil(math.sqrt(a))
-if pow(m,2)==a:
+import math as m
+def root(n):
+    k=m.ceil(m.sqrt(n))
+    if(k**2==n):
+        return True
+    else:
+        return False
+n=int(input())
+if(root(n)==1):
     print("True")
 else:
     print("False")
